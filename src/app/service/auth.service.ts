bf623105bf21dcd133a7c355f6864400b9b70a77
wesley rios <wes.alves.rios@gmail.com>
2022-01-28 14:00:32 -0300

    service postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,12 +14,20 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set("Authorization", environment.token)
+  }
+
   login(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
   return this.http.post<UsuarioLogin>('https://bloghistory.herokuapp.com/usuarios/logar', usuarioLogin)
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://bloghistory.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  getByIdUser(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://bloghistory.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
